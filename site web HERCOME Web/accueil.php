2bd4d91293ff57b738a847b3a1210f1fea523407
Herlin <herlin.simon@gmail.com>
2016-11-14 15:26:02 +0100

    mise a jour 14/11/2016 , site web ajouter, image different diagramme

---
+++ site web HERCOME Web/accueil.php
@@ -0,0 +1,183 @@
+<html lang="en"><head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Neon Admin Panel">
+	<meta name="author" content="">
+
+
+	<title>Hercome | Accueil</title>
+
+
+	<style>.file-input-wrapper { overflow: hidden; position: relative; cursor: pointer; z-index: 1; }.file-input-wrapper input[type=file], .file-input-wrapper input[type=file]:focus, .file-input-wrapper input[type=file]:hover { position: absolute; top: 0; left: 0; cursor: pointer; opacity: 0; filter: alpha(opacity=0); z-index: 99; outline: 0; }.file-input-name { margin-left: 8px; }</style><link rel="stylesheet" href="assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
+	<link rel="stylesheet" href="assets/css/font-icons/entypo/css/entypo.css">
+	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/neon-core.css">
+	<link rel="stylesheet" href="assets/css/neon-theme.css">
+	<link rel="stylesheet" href="assets/css/neon-forms.css">
+	<link rel="stylesheet" href="assets/css/custom.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+
+	<script src="assets/js/jquery-1.11.3.min.js"></script>
+
+<script src="assets/js/ie8-responsive-file-warning.js"></script>
+
+
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+
+</head>
+<body class="page-body loaded" >
+
+<div class="page-container"><!-- add class "sidebar-collapsed" to close sidebar by default, "chat-visible" to make chat appear always -->
+
+	<?php include('menu.php'); ?>
+
+	<div class="main-content">
+		<div class="row">
+
+			<!-- Profile Info and Notifications -->
+			<div class="col-md-6 col-sm-8 clearfix">
+
+
+
+				<ul class="user-info pull-left pull-right-xs pull-none-xsm">
+
+
+
+				</ul>
+
+			</div>
+
+
+			<!-- Raw Links -->
+			<div class="col-md-6 col-sm-4 clearfix hidden-xs">
+
+				<ul class="list-inline links-list pull-right">
+					<li class="sep"></li>
+					<li>
+						<a href="#" data-toggle="chat" data-collapse-sidebar="1">
+							<i class="entypo-user"></i>Profil
+						</a>
+					</li>
+					<li class="sep"></li>
+
+					<li>
+						<a href="page-connexion.html">
+							Déconnection <i class="entypo-logout right"></i>
+						</a>
+					</li>
+				</ul>
+
+			</div>
+
+		</div>
+			<hr>
+				<ol class="breadcrumb bc-3">
+					<li>
+						<a href="accueil.php"><i class="entypo-home"></i>Hercome</a>
+					</li>
+					<li>
+						<a href="accueil.php">Accueil</a>
+					</li>
+				</ol>
+			<h2>Hercome direct</h2>
+			<br>
+
+		<div class="row">
+			<div class="col-sm-3 col-xs-6">
+				<div class="tile-stats tile-red">
+					<div class="icon"><i class="entypo-cloud"></i></div>
+					<div class="num" data-start="0" data-end="24" data-postfix="°" data-duration="1500" data-delay="0">0</div>
+					<h3>Températures ambiante </h3>
+					<p></p>
+				</div>
+			</div>
+
+			<div class="col-sm-3 col-xs-6">
+				<div class="tile-stats tile-green">
+					<div class="icon"><i class="entypo-tools"></i></div>
+					<div class="num" data-start="0" data-end="12" data-postfix="" data-duration="1500" data-delay="600">0</div>
+					<h3>Equipements actifs</h3>
+					<p></p>
+				</div>
+			</div>
+
+			<div class="clear visible-xs"></div>
+			<div class="col-sm-3 col-xs-6">
+				<div class="tile-stats tile-aqua">
+					<div class="icon"><i class="entypo-home"></i></div>
+					<div class="num" data-start="0" data-end="5" data-postfix="" data-duration="1500" data-delay="1200">0</div>
+					<h3>Pièces</h3>
+				</div>
+			</div>
+		</div>
+
+		<div class="text-center" >
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<hr>
+			<h1>Bienvenue sur Hercome , votre site gestionnaire de maison connectée ! </h1>
+			<hr>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+		</div>
+		<!-- Footer -->
+		<footer class="main">
+
+			© 2016-2017 <strong> Hercome</strong>
+
+		</footer>
+	</div>
+
+
+	<hr>
+
+</div>
+
+
+
+
+<!-- Bottom scripts (common) -->
+<script src="assets/js/gsap/TweenMax.min.js"></script>
+<script src="assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+<script src="assets/js/joinable.js"></script>
+<script src="assets/js/resizeable.js"></script>
+<script src="assets/js/neon-api.js"></script>
+
+
+<!-- Imported scripts on this page -->
+<script src="assets/js/neon-chat.js"></script><div id="ascrail2000" class="nicescroll-rails" style="padding-right: 3px; width: 10px; z-index: 5; cursor: default; position: absolute; top: 0px; left: -10px; height: 100px; display: none;"><div style="position: relative; top: 0px; float: right; width: 5px; height: 0px; background-color: rgb(69, 74, 84); border: 1px solid rgb(69, 74, 84); background-clip: padding-box; border-radius: 1px;"></div></div><div id="ascrail2000-hr" class="nicescroll-rails" style="height: 7px; z-index: 5; top: 93px; left: 0px; position: absolute; cursor: default; display: none;"><div style="position: relative; top: 0px; height: 5px; width: 0px; background-color: rgb(69, 74, 84); border: 1px solid rgb(69, 74, 84); background-clip: padding-box; border-radius: 1px;"></div></div>
+
+
+<!-- JavaScripts initializations and stuff -->
+<script src="assets/js/neon-custom.js"></script>
+
+
+<!-- Demo Settings -->
+<script src="assets/js/neon-demo.js"></script>
+
+
+<textarea tabindex="-1" style="position: absolute; top: -999px; left: 0px; right: auto; bottom: auto; border: 0px; padding: 0px; box-sizing: content-box; word-wrap: break-word; overflow: hidden; transition: none; height: 0px !important; min-height: 0px !important; font-family: &quot;Helvetica Neue&quot;, Helvetica, &quot;Noto Sans&quot;, sans-serif; font-size: 12px; font-weight: 400; font-style: normal; letter-spacing: 0px; text-transform: none; word-spacing: 0px; text-indent: 0px; line-height: 17.1429px;" class="autosizejs"></textarea></body></html>
---
+++ site web HERCOME Web/menu.php
@@ -0,0 +1,135 @@
+<div class="sidebar-menu fixed">
+
+	<div class="sidebar-menu-inner ps-container ps-active-y">
+
+		<header class="logo-env">
+			<div class="logo">
+				<a href="http://demo.neontheme.com/dashboard/main/">
+					<img src="img/logo4.png" width="180" alt="">
+				</a>
+			</div> <!-- logo collapse icon -->
+			<div class="sidebar-collapse">
+				<a href="#" class="sidebar-collapse-icon"><!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
+					<i class="entypo-menu"></i>
+				</a>
+			</div> <!-- open/close menu icon (do not remove if you want to enable menu on mobile devices) -->
+			<div class="sidebar-mobile-menu visible-xs">
+				<a href="#" class="with-animation"><!-- add class "with-animation" to support animation -->
+					<i class="entypo-menu"></i>
+				</a>
+			</div>
+		</header>
+
+
+		<ul id="main-menu" class="main-menu" style="">
+			<!-- add class "multiple-expanded" to allow multiple submenus to open -->
+			<!-- class "auto-inherit-active-class" will automatically add "active" class for parent elements who are marked already with class "active" -->
+			<li class="has-sub root-level">
+				<a href="index.html">
+					<i class="entypo-home"></i>
+					<span class="title">Accueil</span>
+				</a>
+
+			</li>
+
+			<li class="has-sub root-level">
+				<a href="index.html" target="_blank">
+					<i class="entypo-bookmarks"></i>
+					<span class="title">Histoire</span>
+				</a>
+			</li>
+			<li class="has-sub root-level ">
+				<a href="layout-api.html">
+					<i class="entypo-layout"></i>
+					<span class="title">Panneau de contrôle</span>
+				</a>
+				<ul >
+					<li class="has-sub">
+						<a href="layout-api.html">
+							<i class="entypo-bookmarks"></i>
+							<span class="title">Cuisine</span>
+						</a>
+					</li>
+					<li class="has-sub">
+						<a href="layout-collapsed-sidebar.html">
+							<i class="fa fa-bed fa-fw"></i>
+							<span class="title">Chambre</span>
+						</a>
+					</li >
+					<li class="has-sub">
+						<a href="accueil.php">
+							<i class="fa fa-cutlery fa-fw"></i>
+							<span class="title">Salle à manger</span>
+						</a>
+					</li >
+					<li class="has-sub">
+						<a href="layout-chat-open.html">
+							<i class="fa fa-car fa-fw"></i>
+							<span class="title">Garage</span>
+						</a>
+					</li>
+					<li class="has-sub">
+						<a href="layout-horizontal-menu-boxed.html">
+							<i class="entypo-water"></i>
+							<span class="title">Salle de bain</span>
+						</a>
+					</li>
+					<li class="has-sub">
+						<a href="layout-horizontal-menu-fluid.html">
+							<i class="fa fa-window-maximize" aria-hidden="true"></i>
+							<span class="title">Cave</span>
+						</a>
+					</li>
+					<li class="has-sub">
+						<a href="layout-mixed-menus.html">
+							<i class="fa fa-gamepad" aria-hidden="true"></i>
+							<span class="title">Salle de jeux</span>
+						</a>
+					</li>
+					<li class="has-sub">
+						<a href="layout-right-sidebar.html">
+							<i class="fa fa-leaf" aria-hidden="true"></i>
+							<span class="title">Extérieur</span>
+						</a>
+					</li>
+
+				</ul>
+			</li>
+			<li class="has-sub root-level">
+				<a href="ui-panels.html">
+					<i class="entypo-chart-line"></i>
+
+					<span class="title">Graphique</span>
+				</a>
+
+			</li>
+			<li class="has-sub root-level">
+				<a href="mailbox.html">
+					<i class="entypo-cloud"></i>
+					<span class="title">Station Meteo</span>
+
+				</a>
+
+			</li>
+			<li class="has-sub root-level">
+				<a href="forms-main.html">
+					<i class="entypo-plus"></i>
+					<span class="title">Ajout de matériel</span>
+				</a>
+
+			</li>
+			<li class="has-sub root-level">
+				<a href="tables-main.html">
+					<i class="entypo-window"></i>
+					<span class="title">Barrière</span>
+				</a>
+
+			</li>
+
+
+
+		</ul>
+
+		<div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 3px;"><div class="ps-scrollbar-x" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; height: 950px; right: 2px;"><div class="ps-scrollbar-y" style="top: 0px; height: 920px;"></div></div></div>
+
+</div>
